e5ee06f8889974e73ef977ac024ef45ca21076e8
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-03-23 21:04:49 +0900

    修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,12 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :nickname, presence: true
   validates :birth_day, presence: true
-  validates :password, length: { minimum: 6 }
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
-  validates :family_name, format: {with: 	/\A[ぁ-んァ-ヶ一-龥]+\z/}
-  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥]+\z/}
+  validates :family_name, format: {with: 	/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
  
-  has_many :purchaser_records
-  has_many :items
+  # has_many :purchaser_records
+  # has_many :items
 end
