ee326679a0afacd8bf31513e44f2eb1a0aceee86
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-03-29 20:40:31 +0900

    Revert "商品の削除機能"
    This reverts commit b3beee6420cdc24de3e2e1d73f0a075795e287ef.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :users
-  resources :items , only: [:index, :new, :create,:show,:edit,:update,:destroy]
+  resources :items , only: [:index, :new, :create,:show,:edit,:update]
   
 
 end
