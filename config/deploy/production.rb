e3be32f056e92bff1eab632f1a260a21913845d1
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-04-13 19:39:51 +0900

    修正④

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '172.31.9.105', user: 'ec2-user', roles: %w{app db web}
+server '18.177.74.52', user: 'ec2-user', roles: %w{app db web}
