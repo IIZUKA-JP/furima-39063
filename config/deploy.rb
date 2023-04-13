859d69e15ecc8fe942676b38287615d5e7605b87
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-04-13 20:25:16 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,10 +11,12 @@ set :rbenv_type, :user
 set :rbenv_ruby, "2.6.5" 
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: {['publickey'],
-                                  keys: ['~/.ssh/furima.pem'],
-                                  forward_agent: true
+set :ssh_options, {
+  auth_methods: ['publickey'],
+  keys: ['~/.ssh/furima.pem'],
+  forward_agent: true
 }
+
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
