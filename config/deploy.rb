f81c2b8694e521d7e64d118be4cba43ee7dd53e6
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-04-14 14:03:27 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.1"
 
 set :application, "furima-39063"
 set :repo_url, "git@github.com:IIZUKA-JP/furima-39063.git"
+set :branch, "main"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,9 +11,11 @@ set :rbenv_type, :user
 set :rbenv_ruby, "2.6.5" 
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options,auth_methods: ['publickey'],
-  keys: ['~/.ssh/furima.pem']
-set :branch, "main"
+set :ssh_options, {
+  auth_methods: ['publickey'],
+  keys: ['~/.ssh/furima.pem'],
+  forward_agent: true
+}
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
