c10dae8763f76ff5c22bf17c3ef771d397f00bd5
IIZUKA-JP <125949828+IIZUKA-JP@users.noreply.github.com>
2023-04-14 15:21:34 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock "~> 3.1"
 
 set :application, "furima-39063"
 set :repo_url, "git@github.com:IIZUKA-JP/furima-39063.git"
-set :branch, "main"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
